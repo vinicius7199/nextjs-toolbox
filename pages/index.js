89daf1fcd3dcc2ba59414efc73903ade8fc2f438
Vinícius Andrade <60449521+vinicius7199@users.noreply.github.com>
2023-04-04 20:21:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,7 @@ export default function Home() {
         <Header title="Next.js Toolbox" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+              Exemplo de formulátio
         </p>
         <FeedbackForm />
         <JokeBlock />
